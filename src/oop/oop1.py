42a6ba2fa33151019898f489cf311fdfc8c83ac5
olympia <owojcik7@gmail.com>
2020-02-14 11:35:29 -0500

    Complete oop1 and oop2 with all passing tests

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,34 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle():
+    """
+    Vehicle is base class for this hierarchy
+    """
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
